273d79c5e9fa1b438605d78c195781c31a17bcca
maersu <me@maersu.ch>
2013-10-31 16:07:59 +0100

    added new relic deploy, added migration to deploy

--- deployit/fabrichelper/taskbase.py
+++ deployit/fabrichelper/taskbase.py
@@ -27,7 +27,6 @@ import urllib
 
 
 class BaseTask(Task):
-
     def __init__(self):
         # see whether the tasks class overrides the hostslist
         try:
@@ -37,7 +36,8 @@ class BaseTask(Task):
 
     def ensure_virtualenv(self):
         if not exists(env.remote_app_path_virtualenv):
-            sudo("%(remote_virtualenv_py)s --no-site-packages --python=python%(python_version)s %(remote_app_path_virtualenv)s" % env)
+            sudo(
+                "%(remote_virtualenv_py)s --no-site-packages --python=python%(python_version)s %(remote_app_path_virtualenv)s" % env)
 
     def virtualenv(self, command):
         sudo("source %s/bin/activate && %s" % (env.remote_app_path_virtualenv, command))
@@ -50,7 +50,7 @@ class BaseTask(Task):
             sudo("find %(remote_app_path)s -name '*.pyc' -print0|xargs -0 rm" % env)
         except:
             pass
-        
+
     def load_site(self, ahost):
         print 'Load %s ...' % ahost
         try:
@@ -65,11 +65,11 @@ class BaseTask(Task):
             return http_code
         except Exception, e:
             print 'EXCEPTION: Could not load site: %s' % (e)
-            
+
     def update_packages(self, pip_filename='requirements.txt'):
         self.adjust_rights('root')
         self.virtualenv('pip install --upgrade pip pyinotify')
-        self.virtualenv('pip install -r %s/%s --upgrade' % (env.remote_app_path, pip_filename)) 
+        self.virtualenv('pip install -r %s/%s --upgrade' % (env.remote_app_path, pip_filename))
         self.adjust_rights()
 
     def deploy_services(self):
@@ -77,7 +77,7 @@ class BaseTask(Task):
             service = service_klass()
             service.deploy()
         self.adjust_rights()
- 
+
     def restart_services(self):
         for service_klass in env.services:
             s = service_klass()
@@ -102,22 +102,58 @@ class Deploy(BaseTask):
         )
         shutil.rmtree(env.local_static_root)
         self.adjust_rights()
-    
+
     def deploy_log(self, message):
         import getpass
-        message = ', '.join([datetime.datetime.now().strftime("%Y-%d-%y %H:%M:%S,%f"), 
-                             getpass.getuser(),
-                             self.get_current_revision(),
+        import dateutil.parser
+        import socket
+
+        try:
+            username = getpass.getuser() + '@' + socket.gethostname()
+        except:
+            username = getpass.getuser()
+
+        revision = self.get_current_revision()
+
+        try:
+            revision_date = local('git show -s --format="%ci" ' + revision, capture=True)
+            revision_date = dateutil.parser.parse(revision_date).strftime("%H:%M %d.%b")
+            changelog = local('git status', capture=True).replace("'", "\\'").replace('"', '\\"')
+        except:
+            revision_date = ''
+            changelog = ''
+
+        message = ', '.join([datetime.datetime.now().strftime("%Y-%d-%y %H:%M:%S,%f"),
+                             username,
+                             revision,
+                             revision_date,
                              message])
+
         sudo("echo '%s' >> %s" % (message, env.remote_path('log', 'deploy.log')))
-        
+
+        # newrelic support
+        if env.has_key('newrelic_application_id') and env.has_key('newrelic_x_api_key'):
+            context = {'application_id': env.newrelic_application_id,
+                       'description': message,
+                       'revision': revision_date or revision,
+                       'user': username,
+                       'changelog': changelog,
+                       'x-api-key': env.newrelic_x_api_key}
+
+            local(
+                'curl --silent -H "x-api-key:%(x-api-key)s" -d "deployment[application_id]=%(application_id)s" '
+                '-d "deployment[description]=%(description)s" -d "deployment[revision]=%(revision)s" '
+                '-d "deployment[changelog]=%(changelog)s" '
+                '-d "deployment[user]=%(user)s" https://rpm.newrelic.com/deployments.xml > /dev/null' % context)
+
+
     def get_current_revision(self):
         with settings(warn_only=True):
             cmd = ''
             with hide('everything'):
                 if local('git status > /dev/null 2>&1', capture=True).succeeded:
                     cmd = 'git rev-parse HEAD'
-                    
+
                 elif local('hg status > /dev/null 2>&1', capture=True).succeeded:
                     cmd = 'hg id -i'
         return local(cmd, capture=True)
@@ -150,8 +186,8 @@ class Deploy(BaseTask):
                         template_dir=env.global_template_dir)
 
     @calc_duration
-    def run(self, no_input=False):
-        
+    def run(self, no_input=False, migrate=False):
+
         self.create_project_directories()
 
         self.initialize_virtualenv()
@@ -168,7 +204,7 @@ class Deploy(BaseTask):
         self.copy_settings_files()
         self.clear_pycs()
         self.adjust_rights()
-        
+
         if self.update_libs:
             self.deploy_services()
         self.restart_services()
@@ -177,7 +213,9 @@ class Deploy(BaseTask):
         status_code = self.load_site("http://%s" % env.server_names[0])
         self.deploy_log(message='%s %s: HTTP status code: %s' % (env.env_name, self.__class__.name, status_code))
 
-        
+        if migrate:
+            Migrate().run()
+
 class FastDeploy(Deploy):
     """
     Deploy all sources to the target
@@ -192,6 +230,7 @@ class VagrantDeploy(Deploy):
     """
     name = "vagrant_deploy"
     update_libs = True
+
     @calc_duration
     def run(self, no_input=False):
         self.create_project_directories()
@@ -209,6 +248,7 @@ class DeployServices(BaseTask):
     Deploy all services necessary to run this project 
     """
     name = "services_deploy"
+
     @calc_duration
     def run(self):
         self.deploy_services()
@@ -219,6 +259,7 @@ class RestartServices(BaseTask):
     Restart all services necessary to run this project 
     """
     name = "services_restart"
+
     @calc_duration
     def run(self):
         self.restart_services()
@@ -229,14 +270,15 @@ class DeploySSLCerts(BaseTask):
     Deploy the given SSL Certs. 
     """
     name = "deploy_sslcerts"
+
     @calc_duration
     def run(self, path):
         if os.path.exists(path):
             print 'Upload certs from %s ...' % path
             put(os.path.join(path, '*.pem'), '/etc/ssl/certs/%s.pem' % env.ssl_cert, use_sudo=True)
             put(os.path.join(path, '*.key'), '/etc/ssl/private/%s.key' % env.ssl_cert, use_sudo=True)
-            self.restart_services()           
-        
+            self.restart_services()
+
         else:
             print 'ERROR: Path not found ' + path
 
@@ -246,12 +288,12 @@ class ResetLoad(BaseTask):
     Reset database
     """
     name = "resetload"
-    
+
     @warning
     @calc_duration
     def run(self, no_input=False):
         with cd(env.remote_path()):
-            self.virtualenv('python -u manage.py resetload -y')
+            self.virtualenv('python -u manage.py resetload')
         self.adjust_rights()
 
 
@@ -262,9 +304,9 @@ class Migrate(BaseTask):
     name = "migrate"
 
     @calc_duration
-    def run(self, app):
+    def run(self, args=''):
         with cd(env.remote_path()):
-            self.virtualenv('python -u manage.py migrate {0}'.format(app))
+            self.virtualenv('python -u manage.py migrate %s' % args)
 
 
 class TailLog(BaseTask):
@@ -272,7 +314,7 @@ class TailLog(BaseTask):
     Show log file
     """
     name = "taillog"
-    
+
     def run(self, no_input=False):
         sudo('tail -f %s --lines=30' % env.remote_path('log', '%(project_name)s.log' % env))
 
@@ -285,14 +327,14 @@ class CheckForUpdates(BaseTask):
 
     def compare_version(self, version_local, version_remote):
         status = 0
-        for l, r in izip_longest(version_local.split('.'), version_remote.split('.')):     
+        for l, r in izip_longest(version_local.split('.'), version_remote.split('.')):
             if l is None:
                 status = 1
                 break
             elif r is None:
                 status = 2
                 break
-            
+
             if l.isdigit() and r.isdigit():
                 l = int(l)
                 r = int(r)
@@ -321,13 +363,13 @@ class CheckForUpdates(BaseTask):
             if not available:
                 # Try to lower
                 available = pypi.package_releases(dist.project_name.lower())
-           
+
             if not available:
                 msg = 'no releases at pypi'
             else:
                 msg = self.compare_version(dist.version, available[0])
             pkg_info = '{dist.project_name} {dist.version}'.format(dist=dist)
-            print '\t{pkg_info:40} {msg}'.format(pkg_info=pkg_info, msg=msg) 
+            print '\t{pkg_info:40} {msg}'.format(pkg_info=pkg_info, msg=msg)
 
 
 class Delete(BaseTask):
@@ -335,7 +377,7 @@ class Delete(BaseTask):
     Delete all files
     """
     name = "delete"
-    
+
     @warning
     @calc_duration
     def run(self):
@@ -359,7 +401,8 @@ class LoadBackup(BaseTask):
 
         use_existing = False
         if os.path.exists(env.backup_local_path):
-            use_existing = confirm('Use existing backup (from %s)?' % time.ctime(os.path.getctime(env.backup_local_path)))
+            use_existing = confirm(
+                'Use existing backup (from %s)?' % time.ctime(os.path.getctime(env.backup_local_path)))
 
         if not use_existing:
             get(backup_remote_path, env.backup_local_path)
