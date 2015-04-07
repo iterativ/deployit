a9b820cdda462a6e45d1a7b9ba2e5a94f59981e2
Pawel Kowalski <pawel.kowalski@iterativ.ch>
2015-04-07 15:48:33 +0200

    version bump

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ for scheme in INSTALL_SCHEMES.values():
 
 setup(
     name='deployit',
-    version='0.6.2',
+    version='0.6.3',
     description="Base web app",
     author='Daniel Egger',
     author_email='daniel.egger@iterativ.ch',
