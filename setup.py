bbecc57364beec1940144e5a6194f963c4538520
Daniel Egger <daniel.egger@gmail.com>
2017-03-22 10:15:05 +0100

    compress sql-backup file before download

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ for scheme in INSTALL_SCHEMES.values():
 
 setup(
     name='deployit',
-    version='0.6.2',
+    version='12.0.1',
     description="Base web app",
     author='Daniel Egger',
     author_email='daniel.egger@iterativ.ch',
