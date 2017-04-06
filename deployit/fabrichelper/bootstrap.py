bb3b974ec90f1108c0d16c9aa4e79d25b53d4534
Pawel Kowalski <pawel.kowalski@iterativ.ch>
2017-04-06 15:49:14 +0200

    install letsencrypt

--- deployit/fabrichelper/bootstrap.py
+++ deployit/fabrichelper/bootstrap.py
@@ -31,6 +31,12 @@ class PuppetBaseTask(Task):
         sudo('apt-get -y install puppet-common')
         sudo('apt-get -y install upstart')
         sudo('apt-get -y install upstart-sysv')
+
+        # nginx setup
+        sudo('apt-get -y install letsencrypt')
+        sudo('openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048')
+        sudo('apt-get -y install nginx')
+
         # get rid of missing hiera file warning
         if not exists('/etc/puppet/hiera.yaml'):
             sudo('touch /etc/puppet/hiera.yaml')
