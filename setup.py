f5175abd9039ed1fbedeea2e9ebd03560247756e
Daniel Egger <daniel.egger@gmail.com>
2017-11-11 11:31:40 +0100

    restart celery

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ for scheme in INSTALL_SCHEMES.values():
 
 setup(
     name='deployit',
-    version='16.5.7',
+    version='16.5.8',
     description="Iterativ GmbH DeployIt",
     author='Daniel Egger',
     author_email='daniel.egger@iterativ.ch',
